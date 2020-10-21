eac8abf22bc39db265e14a6fde8d26ece4653ccd
Ashley Chang <ashley_e_chang@brown.edu>
2020-10-21 17:53:01 -0400

    asdfasdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import './CSS/Contact.css'
 import About from './Pages/About'
 import Experiences from "./Pages/Experiences";
 import Projects from "./Pages/Projects";
 import Contact from "./Pages/Contact";
-import './stars.css'
+import './CSS/stars.css'
 
 class Navbar extends React.Component {
   render(){
@@ -16,8 +17,8 @@ class Navbar extends React.Component {
               </div>
           </li>
           <li className="clickable">
-              <li className="nav3"><h1 style={{color:'none', margin: 0}}><i className="nav fa fa-bars"
-                           style={{fontSize:'35px'}}></i></h1></li>
+              <li className="nav3"><i className="nav fa fa-bars"
+                           style={{fontSize:'35px'}}></i></li>
               <li className="nav2"><a href="#about">About</a></li>
               <li className="nav2"><a href="#experiences">Experiences</a></li>
               <li className="nav2"><a href="#projects">Projects</a></li>
@@ -31,26 +32,38 @@ class Navbar extends React.Component {
 
 function SocialSideBar() {
     return(
-        <div className="navbar">
+        <div className="navbar" style = {{zIndex: 2}}>
         <div className="empty two">
             <h3 className="socials">
-                <a className="link"><i className="fa fa-github"></i></a>
+                <a className="link" href="https://github.com/spswatron"><i className="fa fa-github"></i></a>
                 <a className="link"><i className="fa fa-linkedin"></i></a>
                 <a className="link"><i className="fa fa-instagram"></i></a>
                 <a className="link"><i className="fa fa-envelope"></i></a>
+                <div className={"line"}></div>
             </h3>
         </div>
         </div>
     );
 }
 
-function EmailSideBar() {
+
+
+function ArrowSideBar() {
     return(
-        <div className={"navbar inside-holder empty two"}>
-        <a className="inside column" style={{display: 'flex', alignItems: 'flex-end'}}>
-            <h2><i className="fa fa-chevron-down"></i></h2>
-        </a>
-        </div>
+        <div className={"navbar"} style = {{justifyContent: 'flex-end', zIndex: 2}}>
+            <div className={"empty two"}>
+                <a className={"socials"}>
+                    <a className="link">
+                    <a className="link two"><i className="fa fa-chevron-down"></i></a>
+                    <a className="link two"><i className="fa fa-chevron-down"></i></a>
+
+                    </a>
+
+                    {/*<div className={"link sideways"} style={{textOrientation: 'sideways'}}>ashley_e_chang@brown.edu</div>*/}
+                    <div className={"line"}></div>
+                </a>
+                </div>
+            </div>
     );
 }
 
@@ -60,7 +73,7 @@ class Body extends React.Component {
         <div className="body">
             <div className="page" id="home">
                 <div>
-                    <div className={"row"}>
+                    <div className={"row"} style = {{justifyContent: "center"}}>
                     <h1 style = {{textAlign: "center", display: 'block', marginTop: 0}}>
                         Hi, I'm
                     </h1>
@@ -68,9 +81,16 @@ class Body extends React.Component {
                         <span style={{color: '#ccd6f6'}}> Ashley Chang</span>
                     </h1>
                     </div>
-                    <p style = {{textAlign: "center", marginTop: 0, marginBottom: 0}}>
-                        I'm an aspiring something-something.
+                    <p style = {{textAlign: "center", marginTop: 0, marginBottom: 0, maxWidth: '665px'}}>
+                        I'm a sophomore at Brown University studying Computer Science and English.
+                        I'm an aspiring back-end developer and fiction writer.
                     </p>
+                     <div className={"row"} style = {{justifyContent: "center", alignItems:'center'}}>
+                         <a href="#contact">
+                    <button className="form-field" type="contact" style = {{borderRadius: '10px'}}
+                        href="#contact"> Contact Me </button>
+                         </a>
+                     </div>
                 {/*<h3 className="content">*/}
                 {/*   I'm a sad-end developer study us and economics*/}
                 {/*</h3>*/}
@@ -105,7 +125,7 @@ function App() {
     <div className="App">
         <div className={"top"}>Hi</div>
         <SocialSideBar />
-        <EmailSideBar />
+        <ArrowSideBar />
         <Stars/>
         <Navbar />
         <Body />
