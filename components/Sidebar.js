65663baf2669126d1aad1752b502a008b6b2b5bc
Ashley Chang <ashley_e_chang@brown.edu>
2020-12-23 23:56:04 -0500

    change website to nextjs

---
+++ components/Sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import { Link } from 'react-scroll'
+
+
+class Sidebar extends React.Component {
+    constructor (props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
+  }
+  handleStateChange (state) {
+    this.setState({menuOpen: state.isOpen})
+  }
+    closeMenu () {
+    this.setState({menuOpen: false})
+  }
+    render() {
+        return (
+            <Menu
+                width={ '220px' }
+                isOpen={this.state.menuOpen}
+                onStateChange={(state) => this.handleStateChange(state)}
+                right>
+              <li className="nav3 nav fa"/>
+              <li className="nav2"> <Link activeClass="active" to="about" spy={true} smooth={true} offset={0} duration={300} onClick={() => this.closeMenu()}>About</Link></li>
+              <li className="nav2"><Link activeClass="active" to="experiences" spy={true} smooth={true} offset={-25}  duration={300} onClick={() => this.closeMenu()}>Experiences</Link></li>
+              <li className="nav2"><Link activeClass="active" to="projects" spy={true} smooth={true} offset={-25} duration={300} onClick={() => this.closeMenu()}>Projects</Link></li>
+              <li className="nav2"><Link activeClass="active" to="contact" spy={true} smooth={true} offset={-25} duration={300} onClick={() => this.closeMenu()}>Contact</Link></li>
+            </Menu>
+        );
+    }
+}
+export default Sidebar;
---
+++ pages/resume.js
@@ -0,0 +1,5 @@
+export default function resume() {
+    return(
+        <embed src={"/Ashley_Chang_Resume.pdf"}  style={{width:'100%', height: '100%'}} type="application/pdf"/>
+    );
+}
