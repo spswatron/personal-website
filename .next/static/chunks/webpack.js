b985d458a390f8b83cfacfbfab1e8c85c7e470d6
Ashley Chang <ashley_e_chang@brown.edu>
2021-08-03 14:26:37 -0400

    experiences collapsible

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "56c7d7c4b6113d206190"; }
+/******/ 		__webpack_require__.h = function() { return "8bf72c7b22d9e62b205b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
